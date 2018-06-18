5d10dfbeafca634e79621c8c130dc01e220af5b3
KULTI1995 <kulti1995@gmail.com>
2018-06-18 17:36:31 +0200

    fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,15 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyDEz-EKcSblyRInulEnUU8g6qagXiK-4xc',
-    authDomain: 'jobsportal-cb848.firebaseapp.com',
-    databaseURL: 'https://jobsportal-cb848.firebaseio.com',
-    projectId: 'jobsportal-cb848',
-    storageBucket: 'jobsportal-cb848.appspot.com',
-    messagingSenderId: '1051520237224'
+    apiKey: '*****',
+    authDomain: '*****',
+    databaseURL: '*****',
+    projectId: '*****',
+    storageBucket: '*****',
+    messagingSenderId:'*****',
   },
   maps: {
-    apiKey: 'AIzaSyBPp9Trq6KqZh8BK-7Q0Zc_mpzIyqJDalo',
+    apiKey: '*****',
     libraries: ['places']
   }
 };
